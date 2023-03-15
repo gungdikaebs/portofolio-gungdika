5dad447d8be0c78fae4264e1169e6faf1d906e6c
Gung Dika <gungdika88@gmail.com>
2023-03-16 06:40:53 +0800

    CONTACT AND EMAIL

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,6 +58,44 @@ let swiperTestimonial = new Swiper('.testimonial__container', {
 });
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+  contactName = document.getElementById('contact-name'),
+  contactEmail = document.getElementById('contact-email'),
+  contactProject = document.getElementById('contact-project'),
+  contactMessage = document.getElementById('contact-message');
+
+const sendEmail = (e) => {
+  e.preventDefault();
+  // Check if the field has a value
+  if (contactName.value === '' || contactEmail.value === '' || contactProject.value === '') {
+    // Add and remove color
+    contactMessage.classList.remove('color-blue');
+    contactMessage.classList.add('color-red');
+
+    // Show Message
+    contactMessage.textContent = 'write all the input fields📧';
+  } else {
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_aomf08m', 'template_xfglhro', '#contact-form', '8rCchi-yoZ_1yfwx9').then(
+      () => {
+        // show message and add color
+        contactMessage.classList.add('color-blue');
+        contactMessage.textContent = 'Message sent ✅';
+
+        // Remove message after five seconds
+        setTimeout(() => {
+          contactMessage.textContent = '';
+        }, 5000);
+      },
+      (error) => {
+        alert('OOPS! SOMETHING HAS FAILED...', error);
+      }
+    );
+    // To clear the input fields
+    (contactName.value = ''), (contactEmail.value = ''), (contactProject.value = '');
+  }
+};
+contactForm.addEventListener('submit', sendEmail);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
